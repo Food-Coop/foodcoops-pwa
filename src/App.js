aaa3e6eb6f50e98b600bd6978d7fcb131a2e8254
rking <robinking89m@gmail.com>
2021-02-19 22:41:23 +0100

    Added react-router to project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
+import "./App.css";
+import "./Storage.js";
+import "./Home.js";
+import {About} from "./About";
+import {Storage} from "./Storage";
+import {Home} from "./Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <Router>
+            <div className="App">
+                <nav>
+                </nav>
+              <main>
+                <ul>
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/about">Impressum - Legal Disclaimer</Link>
+                  </li>
+                  <li>
+                    <Link to="/storage">Storage</Link>
+                  </li>
+                </ul>
+              </main>
+              <footer>
+              </footer>
+              <Switch>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/storage">
+                  <Storage />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
+        </Router>
+
+    );
 }
 
 export default App;
---
+++ src/AppFooter.js
@@ -0,0 +1,7 @@
+export function AppFooter() {
+        return  (
+            <link>
+                This is a footer.
+            </link>
+        );
+}

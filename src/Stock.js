9d2cb319538c188e49ce288bfb61944bba21a6f0
Stefan Lukas <etaloof@gmail.com>
2021-03-23 23:39:33 +0100

    Format

--- src/Stock.js
+++ src/Stock.js
@@ -8,9 +8,11 @@ import {useExpanded, useTable} from 'react-table'
 
 const Styles = styled.div`
   padding: 1rem;
+
   table {
     border-spacing: 0;
     border: 1px solid black;
+
     tr {
       :last-child {
         td {
@@ -18,12 +20,14 @@ const Styles = styled.div`
         }
       }
     }
+
     th,
     td {
       margin: 0;
       padding: 0.5rem;
       border-bottom: 1px solid black;
       border-right: 1px solid black;
+
       :last-child {
         border-right: 0;
       }
@@ -61,14 +65,14 @@ function reshape(data) {
     return data;
 }
 
-function Table({ columns, data }) {
+function Table({columns, data}) {
     const {
         getTableProps,
         getTableBodyProps,
         headerGroups,
         rows,
         prepareRow,
-        state: { expanded },
+        state: {expanded},
     } = useTable(
         {
             columns: columns,
@@ -77,107 +81,108 @@ function Table({ columns, data }) {
         useExpanded // Use the useExpanded plugin hook
     )
 
-  // Render the UI for your table
-  return (<>
-    <BTable striped bordered hover size="sm" {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody  {...getTableBodyProps()}>
-        {rows.map((row, i) => {
-          prepareRow(row)
-          return (
-            <tr {...row.getRowProps()}>
-              {
-                // canExpand is true for the kategorie header row
-                // make the kategorie name span multiple columns for these rows
-                (row.canExpand ? row.cells.slice(0, 2) : row.cells)
-                .map((cell, i) => {
-                  const props = cell.getCellProps();
-                  if (i === 1 && row.canExpand) {
-                      props.colSpan = row.cells.length - 1;
-                      props.style = props.style || {fontWeight: "bold"};
-                  }
-                return (
-                  <td {...props}>
-                    {cell.render('Cell')}
-                  </td>
-                )
-              })}
-            </tr>
-          )
-        })}
-      </tbody>
-    </BTable>
-          <pre>
-      <code>{JSON.stringify({ expanded: expanded }, null, 2)}</code>
+    // Render the UI for your table
+    return (<>
+            <BTable striped bordered hover size="sm" {...getTableProps()}>
+                <thead>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th {...column.getHeaderProps()}>
+                                {column.render('Header')}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+                </thead>
+                <tbody  {...getTableBodyProps()}>
+                {rows.map((row, i) => {
+                    prepareRow(row)
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {
+                                // canExpand is true for the kategorie header row
+                                // make the kategorie name span multiple columns for these rows
+                                (row.canExpand ? row.cells.slice(0, 2) : row.cells)
+                                    .map((cell, i) => {
+                                        const props = cell.getCellProps();
+                                        if (i === 1 && row.canExpand) {
+                                            props.colSpan = row.cells.length - 1;
+                                            props.style = props.style || {fontWeight: "bold"};
+                                        }
+                                        return (
+                                            <td {...props}>
+                                                {cell.render('Cell')}
+                                            </td>
+                                        )
+                                    })}
+                        </tr>
+                    )
+                })}
+                </tbody>
+            </BTable>
+            <pre>
+      <code>{JSON.stringify({expanded: expanded}, null, 2)}</code>
     </pre>
-          <pre>
+            <pre>
       <code>{JSON.stringify(data, null, 2)}</code>
-    </pre></>
-  )
+    </pre>
+        </>
+    )
 }
 
 export function Stock() {
-  const columns = React.useMemo(
-    () => [
-      //   { // https://github.com/tannerlinsley/react-table/blob/master/examples/expanding/src/App.js
-      //     // https://react-table.tanstack.com/docs/examples/expanding
-      // // Build our expander column
-      //       id: 'expander', // Make sure it has an ID
-      // Header: ({ getToggleAllRowsExpandedProps, isAllRowsExpanded }) => (
-      //     <span {...getToggleAllRowsExpandedProps()}>
-      //       {isAllRowsExpanded ? '👇' : '👉'}
-      //     </span>
-      // ),
-      // Cell: ({ row }) =>
-      //     // Use the row.canExpand and row.getToggleRowExpandedProps prop getter
-      //     // to build the toggle for expanding a row
-      //     row.canExpand ? (
-      //         <span
-      //             {...row.getToggleRowExpandedProps({
-      //               style: {
-      //                 // We can even use the row.depth property
-      //                 // and paddingLeft to indicate the depth
-      //                 // of the row
-      //                 paddingLeft: `${row.depth * 2}rem`,
-      //               },
-      //             })}
-      //         >
-      //         {row.isExpanded ? '👇' : '👉'}
-      //       </span>
-      //     ) : null,
-      //   },
-      {
-      id: 'expander', // Make sure it has an ID
-          Header: ({ getToggleAllRowsExpandedProps, isAllRowsExpanded }) => (
-              <span {...getToggleAllRowsExpandedProps()}>
+    const columns = React.useMemo(
+        () => [
+            //   { // https://github.com/tannerlinsley/react-table/blob/master/examples/expanding/src/App.js
+            //     // https://react-table.tanstack.com/docs/examples/expanding
+            // // Build our expander column
+            //       id: 'expander', // Make sure it has an ID
+            // Header: ({ getToggleAllRowsExpandedProps, isAllRowsExpanded }) => (
+            //     <span {...getToggleAllRowsExpandedProps()}>
+            //       {isAllRowsExpanded ? '👇' : '👉'}
+            //     </span>
+            // ),
+            // Cell: ({ row }) =>
+            //     // Use the row.canExpand and row.getToggleRowExpandedProps prop getter
+            //     // to build the toggle for expanding a row
+            //     row.canExpand ? (
+            //         <span
+            //             {...row.getToggleRowExpandedProps({
+            //               style: {
+            //                 // We can even use the row.depth property
+            //                 // and paddingLeft to indicate the depth
+            //                 // of the row
+            //                 paddingLeft: `${row.depth * 2}rem`,
+            //               },
+            //             })}
+            //         >
+            //         {row.isExpanded ? '👇' : '👉'}
+            //       </span>
+            //     ) : null,
+            //   },
+            {
+                id: 'expander', // Make sure it has an ID
+                Header: ({getToggleAllRowsExpandedProps, isAllRowsExpanded}) => (
+                    <span {...getToggleAllRowsExpandedProps()}>
                 {isAllRowsExpanded ? 'Icon' : 'Icon'}
               </span>
-          ),
-        accessor: 'icon',
-          Cell: ({ cell,row }) => {
-              // Use the row.canExpand and row.getToggleRowExpandedProps prop getter
-              // to build the toggle for expanding a row
-              return row.canExpand ? (
-                  <span
-                      {...row.getToggleRowExpandedProps({
-                          style: {
-                              // We can even use the row.depth property
-                              // and paddingLeft to indicate the depth
-                              // of the row
-                              paddingLeft: `${row.depth * 2}rem`,
-                          }
-                      })}
-                  >
+                ),
+                accessor: 'icon',
+                Cell: ({cell, row}) => {
+                    // Use the row.canExpand and row.getToggleRowExpandedProps prop getter
+                    // to build the toggle for expanding a row
+                    return row.canExpand ? (
+                        <span
+                            {...row.getToggleRowExpandedProps({
+                                style: {
+                                    // We can even use the row.depth property
+                                    // and paddingLeft to indicate the depth
+                                    // of the row
+                                    paddingLeft: `${row.depth * 2}rem`,
+                                }
+                            })}
+                        >
 
                 <div {
                          ...{
@@ -191,45 +196,45 @@ export function Stock() {
                          }
                      } />
                 </span>
-              ) : null;
-          }
-      },
-      {
-        Header: 'Name',
-        accessor: 'name',
-      },
-        {
-        Header: 'Ist Lagerbestand',
-        accessor: 'lagerbestandResource.istLagerbestand',
-        },
-        {
-        Header: 'Soll Lagerbestand',
-        accessor: 'lagerbestandResource.sollLagerbestand',
-        },
-        {
-        Header: 'Einheit',
-        accessor: 'lagerbestandResource.einheit',
-        },
-    ],
-    []
-  );
+                    ) : null;
+                }
+            },
+            {
+                Header: 'Name',
+                accessor: 'name',
+            },
+            {
+                Header: 'Ist Lagerbestand',
+                accessor: 'lagerbestandResource.istLagerbestand',
+            },
+            {
+                Header: 'Soll Lagerbestand',
+                accessor: 'lagerbestandResource.sollLagerbestand',
+            },
+            {
+                Header: 'Einheit',
+                accessor: 'lagerbestandResource.einheit',
+            },
+        ],
+        []
+    );
 
     const [stockData, setStockData] = React.useState([]);
 
     // TODO: use something like https://github.com/rally25rs/react-use-timeout#useinterval or https://react-table.tanstack.com/docs/faq#how-can-i-use-the-table-state-to-fetch-new-data to update the data
     React.useEffect(
-()=>
-    fetch("stock.json")
-        .then((r) => r.json())
-        .then((r) => setStockData(reshape(r))
-        ),[]
-)
+        () =>
+            fetch("stock.json")
+                .then((r) => r.json())
+                .then((r) => setStockData(reshape(r))
+                ), []
+    )
 
-  const data = React.useMemo(() => stockData, [stockData]);
+    const data = React.useMemo(() => stockData, [stockData]);
 
-  return (
-    <div>
-      <Table columns={columns} data={data} />
-    </div>
-  )
+    return (
+        <div>
+            <Table columns={columns} data={data}/>
+        </div>
+    )
 }

ab1bed38022587f3bb33c33e2eb33df7f16694d7
Jonas <jat.schreiner@gmail.com>
2022-03-07 20:54:16 +0100

    Changed keycloak to local

--- src/auth/Keycloak.js
+++ src/auth/Keycloak.js
@@ -3,7 +3,7 @@ import Keycloak from "keycloak-js";
 const keycloakConfig = {
     url: "http://localhost:9090/",
     realm: "foodcoop",
-    clientId: "foodcoop-test"
+    clientId: "foodcoop-pwa"
 }
 
 export const keycloak = new Keycloak(keycloakConfig);
